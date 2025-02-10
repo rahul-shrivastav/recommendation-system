38d0938ca469723c050d380bd5e13d88eb416308
rahul shrivastava <rahulshrivastav052@gmail.com>
2025-02-11 00:52:58 +0530

    fetch

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,28 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import csv
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return "Welcome to the Flask server!"
 
-@app.route('/api/greet', methods=['GET'])
-def greet():
-    name = request.args.get('name', 'Guest')
-    return jsonify({"message": f"Hello, {name}!"})
+@app.route('/api/get_items', methods=['GET'])
+def get_items():
+    try:
+        with open('data/amazon_products.csv', mode='r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            data = [row for i, row in enumerate(reader) if i < 100]
+            return jsonify({"items": data}), 200
+    except FileNotFoundError:
+        print('File not found')
+    except Exception as e:
+        print(e)
 
-@app.route('/api/data', methods=['POST'])
-def receive_data():
-    data = request.json
-    return jsonify({"received": data}), 201
+
+@app.route('/api/recommendation', methods=['GET'])
+def get_recommendations():
+    id = request.get_json()['id']
+    return jsonify({"recommendations": [id]}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { MdCancel } from "react-icons/md";
 import Card from './components/Card';
@@ -6,21 +6,42 @@ import Navbar from './components/Navbar';
 import Rcard from './components/Rcard';
 
 function App() {
-  const [side, setsidebar] = useState(true)
+  const [side, setsidebar] = useState(false)
+  const [items, setitems] = useState(null)
+  const [citem, setcitem] = useState(null)
 
+  useEffect(() => {
+    const helper = async () => {
+      const req = await fetch('http://127.0.0.1:5000/api/get_items');
+      const res = await req.json();
+      const data = await res.items;
+      setitems(data)
+    }
+    helper()
+    console.log(items)
+  }, [])
 
+  if (items) {
   return (
 
     <div className="max-w-screen h-screen flex overflow-x-clip relative ">
-      <div  className={`z-30 bg-white border-2 rounded-2xl border-slate-200 transition-all duration-1000 w-[40vw] h-[90vh] fixed top-[5vh]  ${side?'left-5 ':'-left-[50vw] '} `}></div>
+      {citem &&
+        <div className={`z-30 bg-white border-2 rounded-2xl border-slate-200 transition-all duration-1000 w-[40vw] h-[90vh] fixed top-[5vh]  ${side ? 'left-5 ' : '-left-[50vw] '} `}>
+          <div className="w-[100%] h-[50%] mt-10 ">
+            <img className='w-[40%] m-auto h-full' src={citem.imgUrl} alt="" />
+          </div>
+
+          <div className="w-full h-[40%] border-t border-slate-300 mt-2 "></div>
+        </div >
+      }
       
       <div  className={` z-30 bg-white border-2 rounded-2xl border-slate-200 shadow-sm transition-all duration-1000 w-[55vw] h-[90vh] fixed top-[5vh]   ${side?'right-5 ':'-right-[80vw] ' } `}>
         <button className='w-5 hover:cursor-pointer hover:scale-125 transition-all duration-300 h-5 absolute top-2 right-2 text-xl flex items-center justify-center ' onClick={() => {setsidebar(!side)}}><MdCancel /></button>
         <div className="rounded-xl w-full h-10 bg-white text-black font-extralight text-center flex items-center justify-center">RECOMMONDATIONS</div>
         <div className=" w-full h-[93%] flex items-center justify-center flex-wrap overflow-y-scroll">
           {
-            [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16].map((i) => {
-              return <div onClick={()=>{setsidebar(!side)}} className="w-fit h-fit"><Rcard key={i} /></div> 
+            items.map((i) => {
+              return <div onClick={() => { setcitem(i) }} className="w-fit h-fit"><Rcard url={i.imgUrl} /></div> 
             })
           }
         </div>
@@ -33,20 +54,20 @@ function App() {
 
         <div className="w-full min-h-screen h-fit flex flex-wrap items-center justify-center ">
           {
-            [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16].map((i) => {
-              return <div onClick={()=>{setsidebar(!side)}} className="w-fit h-fit"><Card key={i} /></div> 
+            items.map((i) => {
+              return <div onClick={() => { setsidebar(!side); setcitem(i) }} className="w-fit h-fit"><Card url={i.imgUrl} /></div> 
             })
           }
         </div>
-      </div>
-
-
-      {/* sidebars */}
-      
+      </div>      
 
 
     </div>
   )
+  }
+  else {
+    return <div className="w-screen h-screen flex items-center justify-center">Loading...</div>
+  }
 }
 
 export default App
